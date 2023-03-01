44e9c25d1bfd7877935d3eee5faa9bfae2ce799d
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-03-01 11:06:10 -0500

    Update Property.java
    fixed spaces

--- src/Property.java
+++ src/Property.java
@@ -44,44 +44,44 @@ public class Property {
 
     Property rosie = new Property("Rosie's House", 13, 140, 10, 70, 3.0, "pink", 50);
 
-    Property chrissy = new Property("Chrissy's House", 15, 160, 12, 80, 2.8, "pink", 60);
+    Property chrissy = new Property("Chrissy's House", 14, 160, 12, 80, 2.8, "pink", 60);
    
 
     //orange properties
-    Property bones = new Property("Bones House", 15, 180, 14, 90, 2.5, "orange", 70);
+    Property bones = new Property("Bones House", 16, 180, 14, 90, 2.5, "orange", 70);
 
-    Property cephalobot = new Property("Cephalobot's House", 17, 180, 14, 90, 2.5, "orange", 70);
+    Property cephalobot = new Property("Cephalobot's House", 18, 180, 14, 90, 2.5, "orange", 70);
 
-    Property hopkins = new Property("Hopkin's House", 18, 200, 16, 100, 2.9, "orange", 80);
+    Property hopkins = new Property("Hopkin's House", 19, 200, 16, 100, 2.9, "orange", 80);
 
 
     //red properties
-    Property octavin = new Property("Octavin's House", 19, 220, 18, 110, 2.8, "red", 90);
+    Property octavin = new Property("Octavin's House", 21, 220, 18, 110, 2.8, "red", 90);
 
-    Property fang = new Property("Fang's House", 21, 220, 18, 110, 2.8, "red", 90);
+    Property fang = new Property("Fang's House", 23, 220, 18, 110, 2.8, "red", 90);
 
-    Property kabuki = new Property("Kabuki's House", 22, 240, 20, 120, 2.8, "red", 100);
+    Property kabuki = new Property("Kabuki's House", 24, 240, 20, 120, 2.8, "red", 100);
    
 
     //yellow properties
-    Property stitches = new Property("Stiches's House", 22, 260, 22, 130, 2.8, "yellow", 110);
+    Property stitches = new Property("Stiches's House", 26, 260, 22, 130, 2.8, "yellow", 110);
 
-    Property shino = new Property("Shino's House", 25, 260, 22, 130, 2.8, "yellow", 110);
+    Property shino = new Property("Shino's House", 27, 260, 22, 130, 2.8, "yellow", 110);
 
-    Property bob = new Property("Bob's House", 27, 280, 24, 140, 2.8, "yellow", 120);
+    Property bob = new Property("Bob's House", 29, 280, 24, 140, 2.8, "yellow", 120);
     
 
     //green properties
-    Property francine = new Property("Francine's House", 29, 300, 26, 150, 2.8, "green", 130);
+    Property francine = new Property("Francine's House", 21, 300, 26, 150, 2.8, "green", 130);
 
-    Property judy = new Property("Judy's House", 30, 300, 26, 150, 2.8, "green", 130);
+    Property judy = new Property("Judy's House", 32, 300, 26, 150, 2.8, "green", 130);
 
-    Property diana = new Property("Diana's House", 32, 320, 28, 160, 2.8, "green", 150);
+    Property diana = new Property("Diana's House", 34, 320, 28, 160, 2.8, "green", 150);
     
     
     //dark blue properties
-    Property marshall = new Property("Marshall's House", 35, 350, 35, 175, 2.8, "dark blue", 175);
+    Property marshall = new Property("Marshall's House", 37, 350, 35, 175, 2.8, "dark blue", 175);
     
-    Property raymond = new Property("Raymond's House", 37, 400, 50, 200, 2.8, "dark blue", 200);
+    Property raymond = new Property("Raymond's House", 39, 400, 50, 200, 2.8, "dark blue", 200);
 
 }
