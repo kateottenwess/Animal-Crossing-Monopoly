afe14cbbe922c8a75dff0f155299bd42704b1a11
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-03-14 20:16:08 -0400

    it says i need to commit again

--- src/PropertyType.java
+++
@@ -1,8 +0,0 @@
-abstract class PropertyType {
-    private String propertyName;
-    private int spaceIdentifier;
-    private int purchaseCost;
-    private int rentCost;
-    private int mortage;
-    final double UNMORTAGE_PERC = 1.1;
-}
