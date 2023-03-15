33a8c0f5e4c713860804bff67930e53da81edd9e
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-03-15 10:27:22 -0400

    updteed property

--- src/Property.java
+++ src/Property.java
@@ -8,6 +8,7 @@ public class Property extends BoardSpaceType {
     public int mortgage;
     public int oneHouse;
     public double rentIncreaseRate;
+    public boolean owned;
     final double UNMORTAGE_PERC = 1.1;
     final double RR_INITIAL_COST = 25;
     final double RR_INCREASE = 2;
@@ -15,7 +16,7 @@ public class Property extends BoardSpaceType {
     final int UTILITIES_TWO_OWNED = 10;
 
     public Property(String name, int space, int purchase, int rent, int mortgage, double rentIncreaseRate, String color,
-            int oneHouse) {
+            int oneHouse, boolean owned) {
         this.propertyName = name;
         this.spaceIdentifier = space;
         this.purchaseCost = purchase;
@@ -24,80 +25,81 @@ public class Property extends BoardSpaceType {
         this.rentIncreaseRate = rentIncreaseRate;
         this.color = color;
         this.oneHouse = oneHouse;
+        this.owned = owned;
     }
 
     // brown properties
-    Property rodney = new Property("Rodney's House", 1, 60, 2, 30, 2.0, "brown", 10);
+    Property rodney = new Property("Rodney's House", 1, 60, 2, 30, 2.0, "brown", 10, false);
 
-    Property rocket = new Property("Rocket's House", 3, 60, 4, 30, 2.3, "brown", 20);
+    Property rocket = new Property("Rocket's House", 3, 60, 4, 30, 2.3, "brown", 20, false);
 
     // light blue properties
-    Property melba = new Property("Melba's House", 6, 100, 6, 50, 2.6, "light blue", 30);
+    Property melba = new Property("Melba's House", 6, 100, 6, 50, 2.6, "light blue", 30, false);
 
-    Property marina = new Property("Marina's House", 8, 100, 6, 50, 2.6, "light blue", 30);
+    Property marina = new Property("Marina's House", 8, 100, 6, 50, 2.6, "light blue", 30, false);
 
-    Property mitzi = new Property("Mitzi's House", 9, 120, 8, 60, 2.6, "light blue", 40);
+    Property mitzi = new Property("Mitzi's House", 9, 120, 8, 60, 2.6, "light blue", 40, false);
 
     // pink properties
-    Property flora = new Property("Flora's House", 11, 140, 10, 70, 3.0, "pink", 50);
+    Property flora = new Property("Flora's House", 11, 140, 10, 70, 3.0, "pink", 50, false);
 
-    Property rosie = new Property("Rosie's House", 13, 140, 10, 70, 3.0, "pink", 50);
+    Property rosie = new Property("Rosie's House", 13, 140, 10, 70, 3.0, "pink", 50, false);
 
-    Property chrissy = new Property("Chrissy's House", 14, 160, 12, 80, 2.8, "pink", 60);
+    Property chrissy = new Property("Chrissy's House", 14, 160, 12, 80, 2.8, "pink", 60, false);
 
     // orange properties
-    Property bones = new Property("Bones House", 16, 180, 14, 90, 2.5, "orange", 70);
+    Property bones = new Property("Bones House", 16, 180, 14, 90, 2.5, "orange", 70, false);
 
-    Property cephalobot = new Property("Cephalobot's House", 18, 180, 14, 90, 2.5, "orange", 70);
+    Property cephalobot = new Property("Cephalobot's House", 18, 180, 14, 90, 2.5, "orange", 70, false);
 
-    Property hopkins = new Property("Hopkin's House", 19, 200, 16, 100, 2.9, "orange", 80);
+    Property hopkins = new Property("Hopkin's House", 19, 200, 16, 100, 2.9, "orange", 80, false);
 
     // red properties
-    Property octavin = new Property("Octavin's House", 21, 220, 18, 110, 2.8, "red", 90);
+    Property octavin = new Property("Octavin's House", 21, 220, 18, 110, 2.8, "red", 90, false);
 
-    Property fang = new Property("Fang's House", 23, 220, 18, 110, 2.8, "red", 90);
+    Property fang = new Property("Fang's House", 23, 220, 18, 110, 2.8, "red", 90, false);
 
-    Property kabuki = new Property("Kabuki's House", 24, 240, 20, 120, 2.8, "red", 100);
+    Property kabuki = new Property("Kabuki's House", 24, 240, 20, 120, 2.8, "red", 100, false);
 
     // yellow properties
-    Property stitches = new Property("Stiches's House", 26, 260, 22, 130, 2.8, "yellow", 110);
+    Property stitches = new Property("Stiches's House", 26, 260, 22, 130, 2.8, "yellow", 110, false);
 
-    Property shino = new Property("Shino's House", 27, 260, 22, 130, 2.8, "yellow", 110);
+    Property shino = new Property("Shino's House", 27, 260, 22, 130, 2.8, "yellow", 110, false);
 
-    Property bob = new Property("Bob's House", 29, 280, 24, 140, 2.8, "yellow", 120);
+    Property bob = new Property("Bob's House", 29, 280, 24, 140, 2.8, "yellow", 120, false);
 
     // green properties
-    Property francine = new Property("Francine's House", 31, 300, 26, 150, 2.8, "green", 130);
+    Property francine = new Property("Francine's House", 31, 300, 26, 150, 2.8, "green", 130, false);
 
-    Property judy = new Property("Judy's House", 32, 300, 26, 150, 2.8, "green", 130);
+    Property judy = new Property("Judy's House", 32, 300, 26, 150, 2.8, "green", 130, false);
 
-    Property diana = new Property("Diana's House", 34, 320, 28, 160, 2.8, "green", 150);
+    Property diana = new Property("Diana's House", 34, 320, 28, 160, 2.8, "green", 150, false);
 
     // dark blue properties
-    Property marshall = new Property("Marshall's House", 37, 350, 35, 175, 2.8, "dark blue", 175);
+    Property marshall = new Property("Marshall's House", 37, 350, 35, 175, 2.8, "dark blue", 175, false);
 
-    Property raymond = new Property("Raymond's House", 39, 400, 50, 200, 2.8, "dark blue", 200);
+    Property raymond = new Property("Raymond's House", 39, 400, 50, 200, 2.8, "dark blue", 200, false);
 
     // utilities
     // -1 or NA means it is not applicible for this specific property type
     // waterworks
-    Property ableSis = new Property("Able Sisters", 28, 150, -1, 75, -1, "NA", -1);
+    Property ableSis = new Property("Able Sisters", 28, 150, -1, 75, -1, "NA", -1, false);
 
     // electrical
-    Property nooksCranny = new Property("Nooks Cranny", 12, 150, -1, 75, -1, "NA", -1);
+    Property nooksCranny = new Property("Nooks Cranny", 12, 150, -1, 75, -1, "NA", -1, false);
 
     // railroads
     // railroad one- idk what we want to make it match the theme yet
-    Property rr1 = new Property("Railroad", 5, 200, 25, 100, 2.0, "NA", -1);
+    Property rr1 = new Property("Railroad", 5, 200, 25, 100, 2.0, "NA", -1, false);
 
     // railroad two- idk what we want to make it match the theme yet
-    Property rr2 = new Property("Railroad", 15, 200, 25, 100, 2.0, "NA", -1);
+    Property rr2 = new Property("Railroad", 15, 200, 25, 100, 2.0, "NA", -1, false);
 
     // railroad three- idk what we want to make it match the theme yet
-    Property rr3 = new Property("Railroad", 25, 200, 25, 100, 2.0, "NA", -1);
+    Property rr3 = new Property("Railroad", 25, 200, 25, 100, 2.0, "NA", -1, false);
 
     // railroad four- idk what we want to make it match the theme yet
-    Property rr4 = new Property("Railroad", 35, 200, 35, 100, 2.0, "NA", -1);
+    Property rr4 = new Property("Railroad", 35, 200, 35, 100, 2.0, "NA", -1, false);
 
     // fill the properties array
     public Property getProperty(int spaceIdentifier) {
@@ -160,6 +162,61 @@ public class Property extends BoardSpaceType {
                 return raymond;
             default:
                 return null;
+        }
+    }
+
+    public String getPropertyName() {
+        return this.propertyName;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getSpaceIndentifier() {
+        return this.spaceIdentifier;
+    }
+
+    public int getPurchaseCost() {
+        return this.purchaseCost;
+    }
+
+    public int getRentCost() {
+        return this.rentCost;
+    }
+
+    public int getMortgage() {
+        return this.mortgage;
+    }
+
+    public int getOneHouse() {
+        return this.oneHouse;
+    }
+
+    public double getRentIncreaseRate() {
+        return this.rentIncreaseRate;
+    }
+
+    public boolean isOwned() {
+        return this.owned;
+    }
+
+    public void setOwned(boolean owned) {
+        this.owned = owned;
+    }
+
+    //if 1 is return, the rent cost could be increased, else it cant be increased
+    public int increaseRent(int houseNum) {
+        if (houseNum == 1) {
+            this.rentCost += this.oneHouse;
+            return 1;
+        }
+        else if (houseNum < 5){
+            this.rentCost += this.rentIncreaseRate * this.oneHouse;
+            return 1;
+        }
+        else {
+            return 0;
         }
     }
 }
