34cc66c3729b4ec9348e66ee37c7fd01e92eebf1
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-03-17 09:44:13 -0400

    jail javadoc

--- src/MiscSpaces.java
+++ src/MiscSpaces.java
@@ -5,18 +5,32 @@
  * @version Winter 2023
  **********************************************************************/
 public class MiscSpaces extends BoardSpaceType {
+
+    /* object for the go space */
     public MiscSpaces go;
+
+    /* object for the income tax space */
     public MiscSpaces incomeTax;
+
+    /* object for the free parking space */
     public MiscSpaces freeParking;
+
+    /* object for the go to jail space */
     public MiscSpaces goToJail;
+
+    /* object for the luxury tax space */
     public MiscSpaces luxuryTax;
+
+    /* object for the jail space */
     public MiscSpaces jail;
 
     
-    /** 
+    /***********************************************************
+     * Method that returns the specific mis space from the space       * identifier
+     *  
      * @param spaceIdentifier
      * @return MiscSpaces
-     */
+     ************************************************************/
     public MiscSpaces getType(int spaceIdentifier) {
         switch (spaceIdentifier) {
             case 0:
