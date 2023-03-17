754ff6557bdba1e039b1fd99882754ac23eb39d5
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-03-17 09:19:29 -0400

    finished BoardSpaceType javadoc

--- src/BoardSpaceType.java
+++ src/BoardSpaceType.java
@@ -5,10 +5,17 @@
  * @version Winter 2023
  **********************************************************************/
 public class BoardSpaceType {
-    //i need to look into more i have forgotten so much about inheritance
-    Property p;
-    MiscSpaces m;
+    
+    /* object p to represent type Property */
+    public Property p;
 
+    /* object m to represent type MiscSpaces */
+    public MiscSpaces m;
+
+
+    /*******************************************************************
+    * Constructor that sets p and m to null as their purpose is simply * to signify their type
+    ******************************************************************/
     public BoardSpaceType () {
         p = null;
         m = null;
