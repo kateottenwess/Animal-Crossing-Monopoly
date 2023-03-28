91ccef4593c73d82a879ada313f7739e557839ac
abbyysvec <asveccollege@gmail.com>
2023-03-27 21:33:49 -0400

    community chest & chance rough draft

--- src/Chance.java
+++ src/Chance.java
@@ -5,5 +5,49 @@
  * @version Winter 2023
  **********************************************************************/
 public class Chance extends MiscSpaces {
+
+    String[] chanceCards = {"GO BACK THREE SPACES","ADVANCE TO GO (COLLECT 200 BELLS)","BANK PAYS YOU DIVIDEND OF 50 BELLS","ADVANCE TO FRANCINE'S HOUSE","GET OUT OF JAIL FREE",
+    "ADVANCE TO RAYMOND'S HOUSE","PAY POOR TAX OF 15 BELLS","GO TO JAIL","ADVANCE TOKEN TO CHRISSY'S HOUSE","YOUR BUSINESS MATURES: COLLECT 150 BELLS"};
+
+
+    public String pickChanceCards(){
+        int randInt = (int) (Math.random() * 9);
+        return chanceCards[randInt];
+
+        /* 
+        if (chanceCards[randInt] == chanceCards[0]){
+            int current = Player.getBoardPos();
+            Player.setBoardPos(current - 3);
+        }
+        if (chanceCards[randInt] == chanceCards[1]){
+            Player.setBoardPos(0);
+            Player.setBells(Player.getBells() + 200);
+        }
+        if(chanceCards[randInt] == chanceCards[2]){
+            Player.setBells(Player.getBells() + 50);
+        }
+        if(chanceCards[randInt] == chanceCards[3]){
+            Player.setBoardPos(34);
+        }
+        if(chanceCards[randInt] == chanceCards[4]){
+            //get out of jail
+        }
+        if(chanceCards[randInt] == chanceCards[5]){
+            Player.setBoardPos(34);
+        }
+        if(chanceCards[randInt] == chanceCards[6]){
+            Player.setBells(Player.getBells() - 15);
+        }
+        if(chanceCards[randInt] == chanceCards[7]){
+            //Go to jail
+        }
+        if(chanceCards[randInt] == chanceCards[8]){
+            Player.setBoardPos(11);
+        }
+        if(chanceCards[randInt] == chanceCards[9]){
+            Player.setBells(Player.getBells() + 150);
+        }
+        */
+    }
     //possible chance cards- should probably make like 1 or something
 }
--- src/CommunityChest.java
+++ src/CommunityChest.java
@@ -5,6 +5,70 @@
  * @version Winter 2023
  **********************************************************************/
 public class CommunityChest extends MiscSpaces {
+
+    String[] ccCards = {"GRAND MUSEUM OPENING: COLLECT 50 BELLS","YOU DUG UP A SACK OF BELLS! COLLECT 100 BELLS","GO TO JAIL \n DO NOT PASS GO, DO NOT COLLECT 200 BELLS","GET OUT OF JAIL, FREE!","ADVANCE TO GO: COLLECT 200 BELLS",
+    "RECEIVE 25 BELLS FOR YOUR SERVICES TO THE VILLAGE","INCOME TAX REFUND: COLLECT 20 BELLS","YOU INHERITED 100 BELLS","PAY VILLAGE TAX OF 150 BELLS","BANK ERROR IN YOUR FAVOR: COLLECT 200 BELLS"};
+
+    //cCards[0] = "GRAND MUSEUM OPENING: COLLECT 50 BELLS";
+    //ccCards[1] = "YOU DO UP A SACK OF BELLS! COLLECT 100 BELLS";
+    //ccCards[2] = "GO TO JAIL \n DO NOT PASS GO, DO NOT COLLECT 200 BELLS";
+    //ccCards[3] = "GET OUT OF JAIL, FREE!";
+    //ccCards[4] = "ADVANCE TO GO: COLLECT 200 BELLS";
+    //ccCards[5] = "RECEIVE 25 BELLS FOR YOUR SERVICES TO THE VILLAGE";
+    //ccCards[6] = "INCOME TAX REFUND: COLLECT 20 BELLS";
+   //ccCards[7] = "YOU INHERITED 100 BELLS";
+    //ccCards[8] = "PAY VILLAGE TAX OF 150 BELLS";
+    //ccCards[9] = "BANK ERROR IN YOUR FAVOR: COLLECT 200 BELLS";
     //possible community chest cards- should probably make like 10
+
+    public String pickCCCard(){
+        int randInt = (int) (Math.random() * 9);
+        return ccCards[randInt];
+/* 
+        if (ccCards[randInt] == ccCards[0]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 50);
+        }
+        if (ccCards[randInt] == ccCards[1]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 100);
+        }
+        if (ccCards[randInt] == ccCards[2]){
+            //go to jail
+        }
+        if (ccCards[randInt] == ccCards[3]){
+            //get out of jail
+        }
+        if (ccCards[randInt] == ccCards[4]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 200);
+            //Player.setBoardPos(0);
+        }
+        if (ccCards[randInt] == ccCards[5]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 25);
+        }
+        if (ccCards[randInt] == ccCards[6]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 20);
+        }
+        if (ccCards[randInt] == ccCards[7]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 100);
+        }
+        if (ccCards[randInt] == ccCards[8]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 150);
+        }
+        if (ccCards[randInt] == ccCards[1]){
+            //int bells = Player.getBells();
+            //Player.setBells(Player.getBells() + 200);
+        }*/
+
+
+
+        
+    }
+
     
 }
