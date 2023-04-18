4af3c02235799716bdc9b63a82fd3c3f271d79bf
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2023-04-18 19:33:44 -0400

    Player class cleanup

--- src/Player.java
+++ src/Player.java
@@ -7,36 +7,37 @@ import java.awt.Point;
  * @version Winter 2023
  **********************************************************************/
 public class Player {
-    private String name;
-    private Point coordinates;
-    private int playerNum;
-    private int boardPos;
-    private int bells;
-    // used for property-releated functions
-    private Property p;
 
-    // indices are the space identifiers, the value in the array will be either 0
-    // (false-not owned) or the playernum of who owns it
-    private static int[] properties;
+    /*Name of player*/
+    private String name;
+
+    /*Coordinates of player on board*/
+    private Point coordinates;
+
+    /*Players number*/
+    private int playerNum;
+
+    /*integer value of players board position*/
+    private int boardPos;
+
+    /*Bells amount for player*/
+    private int bells;
+
 
     /*******************************************************************
      * Constructor that initializes the players name, money amount, and
-     * board position
+     * board position.
      ******************************************************************/
     public Player(int playerNum, String name) {
         this.playerNum = playerNum;
         this.name = name;
         this.bells = 1500;
         this.boardPos = 0;
-
-        properties = new int[40];
-        for (int i = 0; i < properties.length; i++) {
-            properties[i] = 0;
-        }
     }
 
+
     /******************************************************************
-     * Getter to return player name
+     * Getter to return player name.
      * 
      * @return String
      ******************************************************************/
@@ -44,17 +45,9 @@ public class Player {
         return name;
     }
 
-    /******************************************************************
-     * Setter to set player name
-     * 
-     * @param name String
-     ******************************************************************/
-    public void setName(final String name) {
-        this.name = name;
-    }
 
     /******************************************************************
-     * Getter to return players board position
+     * Getter to return players board position.
      * 
      * @return int
      ******************************************************************/
@@ -62,8 +55,9 @@ public class Player {
         return boardPos;
     }
 
+
     /******************************************************************
-     * Getter to return the player number
+     * Getter to return the player number.
      * 
      * @return int
      ******************************************************************/
@@ -71,8 +65,9 @@ public class Player {
         return playerNum;
     }
 
+
     /******************************************************************
-     * Setter to set player board position
+     * Setter to set player board position.
      * 
      * @param boardPos int
      ******************************************************************/
@@ -80,8 +75,9 @@ public class Player {
         this.boardPos = boardPos;
     }
 
+
     /******************************************************************
-     * Getter to return player bells
+     * Getter to return player bells.
      * 
      * @return int
      ******************************************************************/
@@ -89,8 +85,9 @@ public class Player {
         return bells;
     }
 
+
     /******************************************************************
-     * Setter to set player bells amount
+     * Setter to set player bells amount.
      * 
      * @param bells int
      ******************************************************************/
@@ -98,105 +95,23 @@ public class Player {
         this.bells = bells;
     }
 
-    /******************************************************************
-     * Method to return if a player has a certain property
-     * 
-     * @param player   player
-     * @param property Property
-     * @return int
-     ******************************************************************/
-    public int hasProperty(final Player player, final Property property) {
-        // wip
-        return 1;
-    }
 
     /******************************************************************
-     * Method to move a player
+     * Setter to set player coordinates for board.
      * 
-     * @param dice1 dice 1 value
-     * @param dice2 dice 2 value
+     * @param p Point
      ******************************************************************/
-    public void move(int dice1, int dice2) {
-        int totalmove = dice1 + dice2;
-
-        if (totalmove + this.getBoardPos() >= 40) {
-            int greater40 = totalmove + this.getBoardPos();
-            int afterGo = greater40 - 40;
-            this.setBoardPos(afterGo - 1);
-            int currentBells = this.getBells();
-            this.setBells(currentBells + 200);
-        } else {
-            this.setBoardPos(this.getBoardPos() + totalmove);
-        }
-    }
-
-
     public void setCoordinates(Point p) {
         coordinates = p;
     }
 
-    public Point getCoordinates() {
-        return coordinates;
-    }
 
     /******************************************************************
-     * Method to purchase a property
+     * Getter to return the players coordinates.
      * 
-     * @param spaceIdentifier int space of property on board
-     * @throws Exception
-     * 
+     * @return Point
      ******************************************************************/
-    // public void buyProperty(int spaceIdentifier) throws Exception {
-
-    //     if (properties[spaceIdentifier] == 0) {
-
-    //         // update properties owned array
-    //         properties[spaceIdentifier] = 1;
-
-    //         // set property to owned
-    //         p.getProperty(spaceIdentifier).setOwned(true, this.playerNum);
-    //     } else {
-    //         throw new Exception("ALREADY OWNED");
-    //     }
-    // }
-
-
-    public void buyHouse(int spaceIdentifier, int numHouses) throws Exception {
-
-        // if the number of houses on a property doesnt exceed 4
-        if (p.getProperty(spaceIdentifier).getNumHouses() + numHouses <= 4) {
-            // if player can afford to get properties
-            if (this.bells - p.getProperty(spaceIdentifier).getOneHouse() * numHouses >= 0) {
-                // if player owns all of the properties of the same color
-                // if (p.allOwned(p.getProperty(spaceIdentifier).getColor()) == this.playerNum) {
-
-                //     // increase number of houses on property
-                //     p.getProperty(spaceIdentifier).increaseNumHouses(numHouses);
-
-                //     // subtract bells from player
-                //     this.bells -= p.getProperty(spaceIdentifier).getOneHouse() * numHouses;
-
-                //     // increase property rent
-                //     p.getProperty(spaceIdentifier).increaseRent(p.getProperty(spaceIdentifier).getNumHouses());
-                // } 
-                // else {
-                //     throw new Exception("ALL COLORS NOT OWNED");
-                // }
-            } else {
-                throw new Exception("CANNOT AFFORD");
-            }
-        } else {
-            throw new Exception("EXCEEDING 4 PROPERTIES");
-        }
-    }
-
-    public int tryBuy(int spaceIdentifier, Property p) {
-      
-        if (properties[spaceIdentifier] == 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-            
+    public Point getCoordinates() {
+        return coordinates;
     }
 }
